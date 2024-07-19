e71f72346fcafd3a160e9ff6b59cfeb443255968
h800 <h800@h800.com>
2024-07-19 20:38:30 +0800

    test h800

--- remoteclip.py
+++ remoteclip.py
@@ -12,5 +12,4 @@ path_to_your_checkpoints = 'weights/models--chendelong--RemoteCLIP/snapshots/bf1
 
 ckpt = torch.load(f"{path_to_your_checkpoints}/RemoteCLIP-{model_name}.pt", map_location="cpu")
 message = model.load_state_dict(ckpt)
-print(message)
 model = model.cuda().eval()
