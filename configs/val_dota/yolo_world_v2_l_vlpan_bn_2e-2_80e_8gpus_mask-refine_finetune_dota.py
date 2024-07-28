08be276bd9cf22fe953db04432e214a4f9c9f545
wd <wdwdwd1024@gmail.com>
2024-07-28 18:17:02 +0800

    some config

--- configs/val_dota/yolo_world_v2_l_vlpan_bn_2e-2_80e_8gpus_mask-refine_finetune_dota.py
+++ configs/val_dota/yolo_world_v2_l_vlpan_bn_2e-2_80e_8gpus_mask-refine_finetune_dota.py
@@ -2,7 +2,7 @@ _base_ = (
     'yolo_world_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_finetune_dota.py')
 
 
-base_lr = 2e-5
+base_lr = 2e-2
 
 # Modify optimizer config
 _base_.optim_wrapper.optimizer.lr = base_lr
