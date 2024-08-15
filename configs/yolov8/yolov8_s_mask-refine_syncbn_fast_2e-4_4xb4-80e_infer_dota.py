8a8a10c73217df13e623717e925eb68c5ad8d781
wmq <wang_mq22@mails.tsinghua.edu.cn>
2024-08-15 08:23:52 -0700

    nx-useless

--- configs/yolov8/yolov8_s_mask-refine_syncbn_fast_2e-4_4xb4-80e_infer_dota.py
+++ configs/yolov8/yolov8_s_mask-refine_syncbn_fast_2e-4_4xb4-80e_infer_dota.py
@@ -2,7 +2,7 @@ _base_ = (
     'yolov8_s_mask-refine_syncbn_fast_2e-4_4xb4-80e_dota.py'
 )
 
-load_from = "/mnt/data1/workspace/wmq/YOLO-World/work_dirs/yolov8_s_mask-refine_syncbn_fast_2e-4_4xb4-80e_dota_train_val/best_dota_mAP_epoch_75.pth"
+load_from = "work_dirs/yolov8_s_mask-refine_syncbn_fast_2e-4_4xb4-80e_dota_train_val/best_dota_mAP_epoch_75.pth"
 
 dota_val_dataset = dict(
         ann_file='val/annfiles/',
