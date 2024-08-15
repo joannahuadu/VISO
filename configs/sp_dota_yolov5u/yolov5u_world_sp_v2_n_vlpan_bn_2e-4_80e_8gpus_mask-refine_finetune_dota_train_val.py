dc91e7fe822167b01cc6b756ee249275c6bcca52
wd <wdwdwd1024@gmail.com>
2024-08-15 08:07:21 +0800

    yolov5_world and split attn

--- configs/sp_dota_yolov5u/yolov5u_world_sp_v2_n_vlpan_bn_2e-4_80e_8gpus_mask-refine_finetune_dota_train_val.py
+++ configs/sp_dota_yolov5u/yolov5u_world_sp_v2_n_vlpan_bn_2e-4_80e_8gpus_mask-refine_finetune_dota_train_val.py
@@ -1,12 +1,11 @@
 _base_ = (
-    '../val_dota/'
-    'yolo_world_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_finetune_dota.py')
+    '../val_dota_yolov5u/'
+    'yolov5u_world_n_vlpan_bn_2e-4_80e_8gpus_mask-refine_finetune_dota_train_val.py')
 
 # hyper-parameters
 train_batch_size_per_gpu = 4
 
-# load_from = 'pretrained_models/yolo_world_l_clip_t2i_bn_2e-3adamw_32xb16-100e_obj365v1_goldg_cc3mlite_train-ca93cd1f.pth'
-load_from = "weights/yolo_world_v2_l_obj365v1_goldg_pretrain_1280ft-9babe3f6.pth"
+# load_from = "weights/yolo_world_v2_l_obj365v1_goldg_pretrain_1280ft-9babe3f6.pth"
 
 # neck_reduce_embed_channels = [256, 512, _base_.last_stage_out_channels]
 neck_reduce_num_heads= [1,1,1] #??
@@ -19,7 +18,7 @@ model = dict(
               reduce_block_cfg=dict(type='KnowledgeAttnBlock'),
             #   downsample_block_cfg=dict(type='DownSampleConvSP')
               ),
-    bbox_head=dict(type='YOLOWorldRotatedHeadSP',
+    bbox_head=dict(type='YOLOWorldHeadSP',
                    ## TODO add configs               
                 ))
 
