3931ce797ed73f8d4bce635abaee0f13d71c7104
wmq <wmq>
2024-08-16 17:22:27 +0800

    yoloworld-s / yoloworldsp-s

---
+++ configs/sp_dota/yolo_world_sp_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_amask_infer_dota_simple_vis.py
@@ -0,0 +1,17 @@
+_base_ = (
+    'yolo_world_sp_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_amask_infer_dota_simple.py'
+)
+neck_reduce_num_heads= [1,1,1] #??
+is_sparse_levels = [0,0,0]
+
+_base_.model.neck.mask_vis = True
+_base_.model.neck.is_sparse_levels = is_sparse_levels
+_base_.model.bbox_head.head_module.is_sparse_levels = is_sparse_levels
+# 这个是把检测结果画出来的hook
+default_hooks = dict(
+    visualization=dict(type='mmdet.engine.hooks.DetVisualizationHook', draw=True)) 
+
+custom_hooks = [ # 加这两个Hook，才能够在推理的时候把mask画出来
+    dict(type='yolo_world.RunnerHook'),
+    dict(type='yolo_world.BatchIdxHook'),
+]
--- configs/sp_dota/yolo_world_sp_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_infer_zero_shot_simple_vis.py
+++ configs/sp_dota/yolo_world_sp_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_infer_zero_shot_simple_vis.py
@@ -1,12 +1,14 @@
 _base_ = (
-    'yolo_world_sp_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_amask_infer_simple_empty.py'
-)
+    'yolo_world_sp_v2_l_vlpan_bn_2e-3_80e_8gpus_mask-refine_infer_zero_shot_simple.py')
+
+
 neck_reduce_num_heads= [1,1,1] #??
 is_sparse_levels = [0,0,0]
 
-_base_.model.neck.mask_vis = True
 _base_.model.neck.is_sparse_levels = is_sparse_levels
 _base_.model.bbox_head.head_module.is_sparse_levels = is_sparse_levels
+
+_base_.model.neck.mask_vis = True # 这个是画特征图和mask的
 # 这个是把检测结果画出来的hook
 default_hooks = dict(
     visualization=dict(type='mmdet.engine.hooks.DetVisualizationHook', draw=True)) 
