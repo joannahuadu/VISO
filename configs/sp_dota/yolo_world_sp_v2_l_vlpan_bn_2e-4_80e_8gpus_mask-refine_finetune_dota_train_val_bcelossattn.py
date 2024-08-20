3b26dbf6b8b90d64f9763a8624f2cbf9a281aa27
wmq <wmq>
2024-08-20 20:24:06 +0800

    configs

--- configs/sp_dota/yolo_world_sp_v2_l_vlpan_bn_2e-4_80e_8gpus_mask-refine_finetune_dota_train_val_bcelossattn.py
+++ configs/sp_dota/yolo_world_sp_v2_l_vlpan_bn_2e-4_80e_8gpus_mask-refine_finetune_dota_train_val_bcelossattn.py
@@ -1,7 +1,7 @@
 _base_ = (
     'yolo_world_sp_v2_l_vlpan_bn_2e-4_80e_8gpus_mask-refine_finetune_dota_train_val.py')
 
-train_batch_size_per_gpu = 1
+train_batch_size_per_gpu = 4
 
 _base_.model.bbox_head.is_split_attn = True
 _base_.model.neck.reduce_block_cfg=dict(type='TextKnowledgeAttnBlock')
--- configs/sp_dota/yolo_world_sp_v2_s_vlpan_bn_2e-3_80e_8gpus_mask-refine_infer_single_task_simple_empty.py
+++ configs/sp_dota/yolo_world_sp_v2_s_vlpan_bn_2e-3_80e_8gpus_mask-refine_infer_single_task_simple_empty.py
@@ -4,24 +4,24 @@ _base_ = (
 
 # neck_reduce_embed_channels = [256, 512, _base_.last_stage_out_channels]
 neck_reduce_num_heads= [1,1,1] #??
-is_sparse_levels = [1,0,0]
-
+is_sparse_levels = [0,0,0]
+num_classes = 1
 load_from = "work_dirs/yolo_world_sp_v2_s_vlpan_bn_2e-4_80e_8gpus_mask-refine_finetune_dota_train_val/best_dota_mAP_epoch_58.pth"
-embedding_path = "tools/embeddings/dota_v1_class_texts_all_embedding.npy"
+embedding_path = "tools/embeddings/dota_v1_class_texts_helicopter_embedding.npy"
 
 # model settings
 model = dict(type='SimpleYOLOWorldDetectorSP',
     mm_neck=True,
     num_train_classes=_base_.num_training_classes,
-    num_test_classes=_base_.num_classes,
+    num_test_classes=num_classes,
     embedding_path=embedding_path,
     prompt_dim=_base_.text_channels,
-    num_prompts=_base_.num_classes,
+    num_prompts=num_classes,
     backbone=dict(with_text_model=False),
     neck=dict(type='YOLOWorldPAFPNSPInfer',
               block_cfg=dict(type='MaxSigmoidCSPLayerWithTwoConvSPInfer', sp_type="vspconv"),
               is_sparse_levels=is_sparse_levels,
-              score_th=0.9,
+              score_th=0.1,
             #   reduce_embed_channels=neck_reduce_embed_channels,
             #   downsample_block_cfg=dict(type='DownSampleConvSPInfer', sp_type="spconv"),
               reduce_num_heads=neck_reduce_num_heads,
@@ -29,6 +29,7 @@ model = dict(type='SimpleYOLOWorldDetectorSP',
     bbox_head=dict(type='YOLOWorldRotatedHeadSPInfer',
                   head_module=dict(type='YOLOWorldRotatedHeadModuleSPInfer',
                                   sp_type="vspconv",
+                                  num_classes=num_classes,
                                   is_sparse_levels = is_sparse_levels))
     )
 
@@ -36,7 +37,9 @@ dota_val_dataset = dict(
     dataset=dict(
         ann_file='val/annfiles/',
         data_prefix=dict(img_path='val/images/'),
-        batch_shapes_cfg=None))
+        batch_shapes_cfg=None),
+    class_text_path='data/texts/dota_v1_class_texts_helicopter.json')
+
 val_dataloader = dict(dataset=dota_val_dataset)
 
 test_dataloader = val_dataloader
