5519fc4b975b5aafcb5f025ecb1c4215c2d7e39f
huadu <joannahuadu@163.com>
2025-05-30 12:02:04 +0800

    qiyuan_metric

--- yolo_world/evaluation/metrics/__init__.py
+++ yolo_world/evaluation/metrics/__init__.py
@@ -3,4 +3,5 @@ from .cloud_metric import CloudMetric
 from .utm_metric import UTMMetric
 from .diorrsvg_metric import DIORRSVGMetric
 from .fair_metric import FAIRMetric
-__all__ = ['OurDOTAMetric', 'CloudMetric', 'UTMMetric', 'DIORRSVGMetric', 'FAIRMetric']
+from .qiyuan_metric import QiyuanMetric
+__all__ = ['OurDOTAMetric', 'CloudMetric', 'UTMMetric', 'DIORRSVGMetric', 'FAIRMetric', 'QiyuanMetric']
