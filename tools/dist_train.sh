9455860a809c494cf0e666098f94cf6d20f8f6b6
huadu <joannahuadu@163.com>
2026-06-10 21:18:02 +0800

    tools and texts

---
+++ tools/dist_train.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+CUDA_VISIBLE_DEVICES=$2
+DEBUG=${3:-0}  # 如果没有传递参数，则将DEBUG设置为默认值0
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${MASTER_PORT:-29518}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+IFS=',' read -ra ADDR <<< "$CUDA_VISIBLE_DEVICES"
+GPUS=${#ADDR[@]}
+echo "Using $GPUS GPUs"
+echo $CUDA_VISIBLE_DEVICES
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES torchrun \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPUS \
+    --master_port=$PORT \
+    $(dirname "$0")/train.py \
+    $CONFIG \
+    # --launcher pytorch ${@:3}
---
+++ tools/generate_text_prompts_list.py
@@ -0,0 +1,58 @@
+import json
+import argparse
+import numpy as np
+from transformers import (AutoTokenizer, CLIPTextModelWithProjection)
+import torch
+import os
+
+def load_model_checkpoint(model, checkpoint_path):
+    checkpoint = torch.load(checkpoint_path, map_location='cuda:0')
+    model_state_dict = checkpoint.get('state_dict', checkpoint)
+    clip_model_state_dict = {k[26:]: v for k, v in model_state_dict.items() if 'text_model' in k}
+
+    model.load_state_dict(clip_model_state_dict, strict=False)
+
+    return model
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model',
+        type=str,
+        default='openai/clip-vit-base-patch32')
+    parser.add_argument(
+        '--checkpoint',
+        type=str,
+        default=None)
+    parser.add_argument('--text',
+                        type=str,
+                        default='data/captions/coco_class_captions.json')
+    parser.add_argument('--out', type=str, default='output.npy')
+
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    model = CLIPTextModelWithProjection.from_pretrained(args.model)
+    if args.checkpoint is not None:
+        load_model_checkpoint(model, args.checkpoint)
+    texts = []
+    prompt = 'Detect the {}'
+    # prompt = '{}'
+    with open(args.text, "r") as file:
+        for line in file:
+            data = json.loads(line)
+            texts.append(prompt.format(data['question']))
+    device = 'cuda:0'
+    model.to(device)
+    txt_feats = []
+    with torch.no_grad():
+        for text in texts:
+            text = tokenizer(text=text, return_tensors='pt', padding=True)
+            text = text.to(device)
+            text_output = model(**text)
+            txt_feat = text_output.text_embeds
+            txt_feat = txt_feat / txt_feat.norm(p=2, dim=-1, keepdim=True)
+            txt_feat = txt_feat.reshape(-1, txt_feat.shape[-1])
+            txt_feats.append(txt_feat)
+            torch.cuda.empty_cache()
+    torch.save(txt_feats, args.out)
